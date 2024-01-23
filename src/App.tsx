74fc055a38b554cf0a55c748edce80c3ee3b5156
pippen <marcus.r@laggelee.se>
2024-01-23 08:52:44 +0100

    routes add to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,72 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import {
   ApolloClient,
   ApolloProvider,
   createHttpLink,
   InMemoryCache,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import "./App.scss";
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import './App.scss'
+import Landing from './Components/Landing/Landing'
 
 export const config: {
-  graphQlUri: string;
-  langUrl: string;
+  graphQlUri: string
+  langUrl: string
 } = {
   graphQlUri: process.env.SERVER_URL
     ? process.env.SERVER_URL
-    : "http://localhost:4000",
+    : 'http://localhost:4000',
   langUrl: process.env.SERVER_URL
     ? `${process.env.SERVER_URL}/language/frontend/{{lng}}`
-    : "http://localhost:4000/language/frontend/{{lng}}",
-};
+    : 'http://localhost:4000/language/frontend/{{lng}}',
+}
 
 const httpLink = createHttpLink({
   uri: `${config.graphQlUri}/graphql`,
-});
+})
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
     },
-  };
-});
+  }
+})
 
 export const graphqlClient = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-});
+})
 
 const App = () => {
   const theme = createTheme({
     palette: {
-      mode: "dark",
+      mode: 'dark',
       background: {
-        default: "#303030",
-        paper: "#383838",
+        default: '#303030',
+        paper: '#383838',
       },
       primary: {
-        main: "#f2800a",
+        main: '#f2800a',
       },
       secondary: {
-        main: "#f2800a",
+        main: '#f2800a',
       },
     },
-  });
+  })
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <ApolloProvider client={graphqlClient}>
         <Routes>
-          <Route />
+          <Route path="/" element={<Landing />} />
         </Routes>
       </ApolloProvider>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/Components/Landing/Landing.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import { Container } from '@mui/system'
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <Container>
+      <Typography variant={'h1'}>{`Hello World!`}</Typography>
+    </Container>
+  )
+}
+export default Landing
